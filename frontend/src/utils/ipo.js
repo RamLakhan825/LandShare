90a62b479a277cdfc0116ee5523374c9d197ac52
Ram Lakhan Jangid <rljangid825@gmail.com>
2025-08-15 17:26:21 +0530

    ram

--- frontend/src/utils/ipo.js
+++ frontend/src/utils/ipo.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const getPendingIPOs = async (token) => {
-  const res = await axios.get("https://landshare-2.onrender.com/ipo/pending", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  const res = await axios.get(`${BASE_URL}/ipo/pending`, {
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
 export const approveIPO = async (id, token) => {
-  const res = await axios.put(`https://landshare-2.onrender.com/ipo/approve/${id}`, null, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  const res = await axios.put(`${BASE_URL}/ipo/approve/${id}`, null, {
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
@@ -21,11 +18,8 @@ export const approveIPO = async (id, token) => {
 
 // src/utils/ipo.js
 export const declineIPO = async (id, token) => {
-  const res = await fetch(`https://landshare-2.onrender.com/ipo/delete/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+  const res = await axios.delete(`${BASE_URL}/ipo/decline/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
   });
   if (!res.ok) throw new Error('Failed to delete IPO');
   return res.json();
